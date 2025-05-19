1fac8f8161e5222527289fea3695bd24dddfb074
michaelgameel5 <michaelgameel15@gmail.com>
2025-05-19 17:14:16 +0300

    Changing Java Version & Adding Members' photos

--- src/main/java/com/example/service/DataService.java
+++ src/main/java/com/example/service/DataService.java
@@ -13,17 +13,17 @@ public class DataService {
 
     public DataService() {
         // AI Team
-        teamMembers.add(new TeamMember("Kamel Ahmed", "Member", "AI Team"));
-        teamMembers.add(new TeamMember("Ahmed Essam", "Member", "AI Team"));
-        teamMembers.add(new TeamMember("Yassin Shaher", "Member", "AI Team"));
+        teamMembers.add(new TeamMember("Kamel Ahmed", "Member", "AI Team", "/photos/kamel.jpg"));
+        teamMembers.add(new TeamMember("Yassin Shaher", "Member", "AI Team", "/photos/yassin.jpg"));
+        teamMembers.add(new TeamMember("Ahmed Essam", "Member", "AI Team", "/photos/essam.jpg"));
         // Monitoring Team
-        teamMembers.add(new TeamMember("Seif Waheed", "Member", "Monitoring Team"));
-        teamMembers.add(new TeamMember("Dalia Ali", "Member", "Monitoring Team"));
-        teamMembers.add(new TeamMember("Mohamed Hany", "Member", "Monitoring Team"));
+        teamMembers.add(new TeamMember("Mohamed Hany", "Member", "Monitoring Team", "/photos/hany.jpg"));
+        teamMembers.add(new TeamMember("Seif Waheed", "Member", "Monitoring Team", "/photos/seif.jpg"));
+        teamMembers.add(new TeamMember("Dalia Ali", "Member", "Monitoring Team", "/photos/dalia.jpg"));
         // Security Team
-        teamMembers.add(new TeamMember("Omar Abdo", "Member", "Security Team"));
-        teamMembers.add(new TeamMember("Mohamed Hesham", "Member", "Security Team"));
-        teamMembers.add(new TeamMember("Michael Gameel", "Member", "Security Team"));
+        teamMembers.add(new TeamMember("Omar Abdo", "Member", "Security Team", "/photos/omar.jpg"));
+        teamMembers.add(new TeamMember("Mohamed Hesham", "Member", "Security Team", "/photos/hesham.jpg"));
+        teamMembers.add(new TeamMember("Michael Gameel", "Member", "Security Team", "/photos/michael.jpg"));
     }
 
     public List<TeamMember> getTeamMembers() {
